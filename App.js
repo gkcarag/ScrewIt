77484d8c522a683b5f4ef6ea28d22121393a2b6e
gkcarag <gkcarag@gmail.com>
2021-10-30 19:16:25 -0700

    testing again

--- App.js
+++ App.js
@@ -5,8 +5,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ScrewIt!</Text>
       <StatusBar style="auto" />
+      <View style={styles.icon1}></View>
+      <View style={styles.icon2}></View>
+      
+      
     </View>
   );
 }
@@ -14,8 +17,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexWrap: 'wrap',
+    flexDirection: 'row',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
+  icon1: {
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderWidth: 2,
+    width: 100,
+    height: 100,
+    backgroundColor: "lightblue",
+    borderBottomLeftRadius:20,
+    borderBottomRightRadius:20,
+    borderTopLeftRadius:20,
+    borderTopRightRadius:20
+  },
+  icon2: {
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderWidth: 2,
+    width: 100,
+    height: 100,
+    backgroundColor: "lightgreen",
+    borderBottomLeftRadius:20,
+    borderBottomRightRadius:20,
+    borderTopLeftRadius:20,
+    borderTopRightRadius:20
+  }
 });
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # ScrewIt
 Mobile party game application for iOS and Android
-testing github alert
+testing github
