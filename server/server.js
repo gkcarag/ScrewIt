8f0b873d01ae10aab1b716da45382fb4e2074b49
ppark <patparkk@gmail.com>
2021-11-18 11:57:25 -0800

    post, put, delete commands added to server.js; still unfunctional, needs testing

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 // protocode for server connectivity
 // currently using express.js and mysql
+// may need to install mysqli (mysql might be deprecated)
+
 const express = require('express');
 const app = express();
 
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host:'localhost',
+    host:'localhost', //will need to change this to phone device
     user:'root',
     password:'',
     database:'testdb'
@@ -26,8 +28,9 @@ connection.connect(function(err){
     console.log('connected as id ' + connection.threadId);
 });
 
+// need to test the below commands, sql statements not properly coded yet
 app.get('/users', function(req,res){
-    connection.query('select * from users', function(error,rows, fields){
+    connection.query('select * from users', function(error,rows,fields){
         if(error) console.log(error);
 
         else{
@@ -36,3 +39,36 @@ app.get('/users', function(req,res){
         }
     })
 });
+
+app.post('/users', function(req,res){
+    connection.query('insert into', function(error,rows,fields){
+        if(errror) console.log(error);
+
+        else{
+            console.log(rows);
+            res.send(rows);
+        }
+    })
+});
+
+app.put('/users', function(req,res){
+    connection.query('update', function(error,rows,fields){
+        if(error) console.log(error);
+
+        else{
+            console.log(rows);
+            res.send(rows);
+        }
+    })
+});
+
+app.delete('/users', function(req,res){
+    connection.query('delete from ', function(error,rows,fields){
+        if(error) console.log(error);
+
+        else{
+            console.log(rows);
+            res.send(rows);
+        }
+    })
+})
