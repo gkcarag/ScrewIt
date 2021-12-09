d3d2ad5564e231876dee08318e6f9c0387b67a36
LuisAguilarSE <luissaguilar32@gmail.com>
2021-12-09 12:18:36 -0800

    Added Client library for Socket.io and dependincies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-//testing push notifications for discord
+//Socket.io connectivity
+
+var app = require('express')();
+var http = require('http').createServer(app);
+const PORT = 8080;
+
+http.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
+});
+
+io.on('connection', (socket) => { /* socket object may be used to send specific messages to the new connected client */
+
+    console.log('new client connected');
+});
