b69037fe318769716293a84d9f988b5ab17390c0
ppark <patparkk@gmail.com>
2022-03-03 13:05:06 -0800

    changed App.js from function to class

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import {Component} from 'react';
+window.navigator.userAgent = 'react-native';
 import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 import Navigator from './src/screens/home/stack';
 
-export default function App() {
-  return (
-    <Navigator />
-  );
+export default class App extends Component{
+  constructor(){
+    super();
+  }
+  render(){
+    return(
+      <Navigator/>
+    );
+  };
 };
