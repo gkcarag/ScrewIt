48f80b5da466426c86466e191eb95704bd3e632d
ppark <patparkk@gmail.com>
2022-03-08 13:26:39 -0800

    moved test1.js to chat.js, added raptest.js to work on reading text files

---
+++ src/screens/games/RapBattle/raptest.js
@@ -0,0 +1,12 @@
+import { SafeAreaView } from "react-native";
+import { Text, Button } from "react-native-paper";
+import React from "react";
+
+const raptest = (props) => {
+    return(
+        // write function that reads text file and display random phrase to screen
+        <div></div>
+    )
+};
+
+export default raptest;
--- src/screens/home/chat.js
+++ src/screens/home/chat.js
@@ -1,19 +1,55 @@
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, TextInput } from "react-native";
 import { Text, Button } from "react-native-paper";
 import React from "react";
+import { Component } from "react";
+import io from "socket.io-client";
 
-//gonna need props for navigation to other pages
-const chat = (props) => {
-    return(
-        <SafeAreaView>
-            <Text>
-                Chat Testing
-            </Text>
-            <Button onPress={() => props.navigation.goBack()}>
-                Go Back
-            </Button>
-        </SafeAreaView>
-    )
-};
+export default class chat extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            chatMessage: "",
+            chatMessages: []
+        }
+    }
 
-export default chat;
+    //chat app functional between multiple users, but only locally for now
+    componentDidMount(){
+        this.socket = io("http://192.168.1.13:3000"); //replace parameter with your own local ip
+        this.socket.on("chat message", msg => {
+            this.setState({ chatMessages: [...this.state.chatMessages, msg] });
+        });
+    }
+
+    submitChatMessage(){
+        this.socket.emit("chat message", this.state.chatMessage);
+        this.setState({chatMessage: ""});
+    }
+
+    render(){
+        const chatMessages = this.state.chatMessages.map(chatMessage => <Text key={chatMessage}>{chatMessage}</Text>)
+        return(
+            <SafeAreaView>
+                <Text>
+                    testing chat screen
+                </Text>
+                <TextInput
+                    style={{height:40, borderWidth: 2}}
+                    autoCorrect ={false}
+                    value={this.state.chatMessage}
+                    onSubmitEditing={() => this.submitChatMessage()}
+                    onChangeText={chatMessage => {
+                        this.setState({ chatMessage });
+                    }}
+                />
+                <Button onPress={() => this.props.navigation.goBack()}>
+                    Go Back
+                </Button>
+                <Button onPress={() => this.props.navigation.goBack()}>
+                    nothing button
+                </Button>
+                {chatMessages}
+            </SafeAreaView>
+        );
+    }
+}
