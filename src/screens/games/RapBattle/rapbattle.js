d787d143aa7789081d381455b5212ed7df252f74
LuisAguilarSE <luissaguilar32@gmail.com>
2022-03-23 10:05:47 -0700

    Reworking RabBattle

--- src/screens/games/RapBattle/rapbattle.js
+++ src/screens/games/RapBattle/rapbattle.js
@@ -1,43 +1,66 @@
 import { SafeAreaView, Dimensions, StyleSheet, View } from "react-native";
-import { Text, Button } from "react-native-paper";
-import React, { useState } from "react";
-//import TrackPlayer, {RepeatMode} from 'react-native-track-player';
+import { Text, Button, TextInput } from "react-native-paper";
+import React, { Component, useState } from "react";
 
 import Phrases from './rbPhrases';
 export const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT} = Dimensions.get('window');
 
-//Scores
-export const SCORE = 1;
-const rapbattle =  (props) => {
+//const [index, setIndex] = useState();
+//const gen = () =>{
+//    const index = Math.floor(Math.random() * Phrases.length);
+//    setIndex(index);
+//}
+export default function rapbattle(){
 
-    //await TrackPlayer.setupPlayer({});
+  const [started, setStarted] = useState(false);
+  const [status, setStatus] = useState(false);
+  const [phrase, setPhrase] = useState();
+  
+  console.log(phrase);
 
-    //await TrackPlayer.add({
-     // url: require('./rapMusic/Duel_of_the_Fates.mp3')
-   // });
-
-    //TrackPlayer.setRepeatMode(RepeatMode.Queue);
-
-
-    const [index, setIndex] = useState();
-    const gen = () =>{
-        const index = Math.floor(Math.random() * Phrases.length);
-        setIndex(index);
+  const submitPhrase = (p) => {
+    p.preventDefault();
+    const valid = phrase != null;
+    if(!valid){
+      return;
+    }else{
+      setStatus("Rap Phrase Submitted!")
     }
+  };
+
+  const start = () => {
+    const pr = (Math.floor(Math.random() * Phrases.length));
+    setPhrase(pr);
+    setStarted(true);
+  }
+
+  if(started){
     return(
-        <View style={styles.contentView}>
-        <SafeAreaView>
-            <Text style={styles.subHeader}>YOU WORD: {Phrases[index] && Phrases[index].phrase}</Text>
-            <View style={styles.buttonsContainer}>
-              <Button style={styles.gen} onPress = {gen}>Generate Word</Button>
-            </View>
-            <Button style={styles.goBack} onPress={() => props.navigation.goBack()}>
-                Go Back
-            </Button>
-        </SafeAreaView>
-        </View>
+      <View style={styles.contentView}>
+      <SafeAreaView>
+          <Text style={styles.subHeader}>YOUR WORD: </Text>
+          <View style={styles.buttonsContainer}>
+            <Button style={styles.gen} onPress = {submitPhrase}>Submit Rap Phrase</Button>
+            <TextInput string={phrase} onChange={(p) => setPhrase(p.target.value)}/>
+          </View>
+          <Button type="submit">Submit</Button>
+          <Button style={styles.goBack} onPress={() => this.props.navigation.goBack()}>
+              Go Back
+          </Button>
+          {status}
+      </SafeAreaView>
+      </View>
+  )
+  }else{
+    return(
+      <SafeAreaView>
+        <Button type="button" onPress={start}> START GAME</Button>
+        {status}
+      </SafeAreaView>
     )
-};
+  }
+}
+
 
 const styles = StyleSheet.create({
     contentView: {
@@ -76,4 +99,3 @@ const styles = StyleSheet.create({
       textShadowColor : "white",
     }
   });
-export default rapbattle;
