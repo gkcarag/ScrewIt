969e311f3d4475087186657a120072f262a1ad92
ppark <patparkk@gmail.com>
2022-03-23 17:05:22 -0700

    new packages and testing crud code in signup.js

--- src/screens/auth/signup.js
+++ src/screens/auth/signup.js
@@ -5,6 +5,62 @@ import { useForm, Controller } from 'react-hook-form';
 import FormInput from '../components/FormInput';
 import FormButton from '../components/FormButton';
 
+// ** edits made by patrick, to test crud requests **
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("../server/db");
+    
+// middleware
+app.use(cors());
+app.use(express.json()); //req.body
+
+// queries
+// add new user
+/*
+app.post("/adduser", async(req,res) => {
+    try {
+        //description is in json format
+        //const { description } = req.body;
+        const newUser = await pool.query(
+            "INSERT INTO USER_INFO (id, username, email, pword) VALUES ($1,$2,$3,$4)",
+            //[description]
+        );
+
+        res.json(newUser.rows[0]);
+        console.log(req.body);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+// select all users
+app.get("/getUsers", async(req,res) => {
+    try{
+        const allUsers = await pool.query("SELECT * FROM USER_INFO");
+        res.json(allUsers.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+*/
+
+// testing out below command first
+/*
+app.get("/getUser/:username", async(req,res) => {
+    try{
+        const { id } = req.params;
+        const getUser = await pool.query(
+            "SELECT * FROM USER_INFO WHERE username = $1", [username]);
+        res.json(getUser.rows[0]);
+        
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+*/
+// ** end edits **
+
 const signup = () => {
 
     const submitPress = data => {
