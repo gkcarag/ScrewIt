0a7f04f7d35c61e4dbb771fb4448156527811a06
LuisAguilarSE <luissaguilar32@gmail.com>
2022-05-01 03:05:16 -0700

    Small upate to profile. 'Comming Soon'

--- src/screens/home/profile.js
+++ src/screens/home/profile.js
@@ -2,6 +2,8 @@ import { SafeAreaView } from "react-native";
 import { Text, Button } from "react-native-paper";
 import { StyleSheet, View, TouchableHighlight, ImageBackground, Image } from 'react-native';
 import React from "react";
+import styles from "../styles";
+
 import { TabActions } from "@react-navigation/native";
 import gameLibrary from "./gameLibrary"
 
@@ -12,27 +14,18 @@ const profile = (props) => {
         <View style={{
             flex: 1, 
             alignItems:'center',
-            
         }}>
-            <Image style={styles.logo} source={require('../../../assets/default_profile_pic.png')}/>
+        <Image style={styles.plogo} source={require('../../../assets/default_profile_pic.png')}/>
         </View>
-        <SafeAreaView>
-            <Text>
-                Profile
+
+            <Text style={styles.title}>
+                COMMING SOON
             </Text>
-            <Button style={styles.buttons}onPress={() => props.navigation.goBack()}>
+            <Button style={styles.button}onPress={() => props.navigation.goBack()}>
                 Go Back
             </Button>
-        </SafeAreaView>
         </ImageBackground>
     )
 };
-
-const styles = StyleSheet.create({
-    buttons: {
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
 
 export default profile;
