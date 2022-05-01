668b891fe1201777c4cf875e06853f451ce048ee
gkcarag <gkcarag@gmail.com>
2022-05-01 16:52:55 -0700

    waiting room again

---
+++ src/screens/games/RapBattle/rapWait.js
@@ -0,0 +1,17 @@
+import { SafeAreaView, Modal, View, Alert, StyleSheet, Pressable, ImageBackground } from "react-native";
+import { Text, Button, TextInput } from "react-native-paper";
+import React from "react";
+import { Component } from "react";
+import io from "socket.io-client";
+import styles from "../../styles";
+
+export default class rapWait extends Component {
+
+    render() {
+        return(
+            <SafeAreaView>
+
+            </SafeAreaView>
+        );
+    }
+}
